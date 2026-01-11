0e14063ce220aa4f2267b8d8b24a01840b274e59
xiumu590-droid <xiumu590@gmail.com>
2026-01-11 23:37:52 +0800

    Delete git.sh

--- git.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-REPO=git@github.com:xiumu590-droid/xiumu.git
-BRANCH=main
-git config --global user.name "xiumu590-droid"
-git config --global user.email "xiumu590@gmail.com"
-git add *
-git commit -m "上传 $(date +'%m%d-%H%M%S')" || echo "无变动"
-git push origin "$BRANCH"
